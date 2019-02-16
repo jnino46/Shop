c9e65c9b549e20a8360fcc2233233fb8e6231d12
JAIRO ADOLFO NI�O MAYORGA <jaironino229822@correo.itm.edu.co>
2019-02-16 18:00:41 -0500

    add SeedDB

---
+++ Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,44 @@
+﻿namespace Shop.Web.Data
+{
+    using Entities;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("Asus 8gb");
+                this.AddProduct("Equipo Lg");
+                this.AddProduct("Desktop DELL");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(100),
+                IsAvailabe = true,
+                Stock = this.random.Next(100)
+            });
+        }
+    }
+
+}
