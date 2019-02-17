90234c147641a9d9c67f31403ad8cce0486e8773
JAIRO ADOLFO NI�O MAYORGA <jaironino229822@correo.itm.edu.co>
2019-02-17 14:58:22 -0500

    Add generic repository and countries entity

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace Shop.Web.Data
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
-    using Shop.Web.Data.Entities;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Entities;
 
-    public class DataContext : DbContext
+
+    public class DataContext : IdentityDbContext<User>
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Country> Countries { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -1,18 +1,22 @@
 ﻿namespace Shop.Web.Data
 {
-    using Entities;
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using Entities;
+    using Microsoft.AspNetCore.Identity;
+    using Helpers;
+    
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly IUserHelper userHelper;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IUserHelper UserHelper )
         {
             this.context = context;
+            userHelper = UserHelper;
             this.random = new Random();
         }
 
@@ -20,23 +24,43 @@
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userHelper.GetUserByEmailAsync("jairo.nino.toto@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Jairo",
+                    LastName = "Niño",
+                    Email = "jairo.nino.toto@gmail.com",
+                    UserName = "jairo.nino.toto@gmail.com",
+                    PhoneNumber = "3217854453"
+                };
+
+                var result = await this.userHelper.AddUserAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("Asus 8gb");
-                this.AddProduct("Equipo Lg");
-                this.AddProduct("Desktop DELL");
+                this.AddProduct("Asus 8gb", user);
+                this.AddProduct("Equipo Lg", user);
+                this.AddProduct("Desktop DELL", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(100),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User = user
             });
         }
     }
